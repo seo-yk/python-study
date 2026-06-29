3c0535858b2e67f38f47442b92c0ad816893c6e8
Youkyung Seo <seoyk.dev@gmail.com>
2026-06-29 11:56:14 +0900

    Time: 47 ms (8.62%), Space: 30 MB (96.12%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,22 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # time complexity: O(N)
+        """complexity
+            - 정렬: O(n log n)
+            - 반복문: O(n)
+            - nums.length: 최소 O(n log n)
+            - sorted() 대신 sort(): 새 배열 대신 원본 배열 정렬 
 
-        # 2회 이상 보이면 return
-        # 1. set을 통해 중복 제거 후 길이 비교
+            문제: 반복 시 return true
+            1. nums 배열 제자리 정렬
+            2. 0부터 len(nums)-2까지 순회
+            3. 현재 값과 다음 값이 같으면 True
+            4. 끝까지 없으면 False
+        """
 
-        return len(nums) != len(set(nums))
+        nums.sort()
+
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+
+        return False
